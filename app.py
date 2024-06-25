6ec63884e91d8655f754f6f5a71de52748e9d7d3
Hanna <h.rabitha@gmail.com>
2024-06-25 07:14:59 +0700

    init

--- app.py
+++ app.py
@@ -8,10 +8,6 @@ from distutils.log import debug
 from fileinput import filename 
 from flask import *  
 
-import classification as clf
-
-cls = clf.Classification()
-
 
 app = Flask(__name__)
 
@@ -30,89 +26,28 @@ def success():
         path_name = os.path.join("static/uploads", f.filename)
 
         print(path_name)
-        prediction_result = cls.start_classification(path_name)
-
-        if prediction_result == 0:
-            prediction = "Mangkok"
-        elif prediction_result == 1:
-            prediction = "Oval"
-        else:
-            prediction = "Sudut"
-
-        print(prediction)
-        print(prediction_result)
-        data_result = {
-            "msg": "success",
-            "name": f.filename,
-            "prediction_result": str(prediction_result),
-            "prediction": prediction
-        }
-
-        # return jsonify(data_result)
+       
         return render_template("result.html", name = f.filename)  
 
 
-
-# endpoint return json data
-@app.route('/walet', methods = ['POST'])
-def upload_walet():   
-    if request.method == 'POST':   
-        print(request.files['file'])
-        f = request.files['file']
-
-        
-        f.save(os.path.join("static/uploads", f.filename))
-
-        path_name = os.path.join("static/uploads", f.filename)
-
-        print(path_name)
-        
-
-        # prediction = "Mangkok" if prediction_result == 0 else "Oval" if prediction_result == 1 else "Sudut"
-
-        # prediction_result = int(clf.classification(f.filename))
-        prediction_result = cls.start_classification(path_name)
-        
-        if prediction_result == 0:
-            prediction = "Mangkok"
-        elif prediction_result == 1:
-            prediction = "Oval"
-        else:
-            prediction = "Sudut"
-        
-        print(prediction_result)
-        print(prediction)
-
-        data_result = {
-            "msg": "success",
-            "name": f.filename,
-            "prediction_result": str(prediction_result),
-            "prediction": prediction
-        }
-
-        return jsonify(data_result)
-        
-
-
-
 # EXAMPLE CODE ==========================
-# stores = [
-#     {
-#     "name": "Ekaabo Cosmetics",
-#     "items":[
-#     {"name" : "lip gel",
-#      "price" : 20.36
-#      },
-#     {"name" : "moisturizer",
-#      "price" : 38.90
-#      }, 
-#      {"name" : "hair wigs",
-#       "price" : 50.89
-#       },
-#       {"name" : "Body Cream",
-#        "price": 12.46}]
-#     }
-#     ]
+stores = [
+    {
+    "name": "Ekaabo Cosmetics",
+    "items":[
+    {"name" : "lip gel",
+     "price" : 20.36
+     },
+    {"name" : "moisturizer",
+     "price" : 38.90
+     }, 
+     {"name" : "hair wigs",
+      "price" : 50.89
+      },
+      {"name" : "Body Cream",
+       "price": 12.46}]
+    }
+    ]
 
 # METHOD GET DATA
 
@@ -130,7 +65,6 @@ def post_store():
         "items":[]
         }
     image = stores.append(new_store) #append new store to the list
-    prediction_result = clf.classification(image)
     return new_store, 201 #return the store and status code
 
 
